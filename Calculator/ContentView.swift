26eb8b05d674d7a66d626648d07c8739fd3a023f
liulu <liulunet@live.com>
2023-08-30 23:22:04 +0800

    CalculatorButtionItem,Color

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let row: [CalculatorButtonItem] = [
+        .digit(1), .digit(2), .digit(3), .op(.plus)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            ForEach(row, id: \.self) { item in
+                CalcularotButton(title: item.title, size: item.size, backgroundColorName: item.backgroundColorName) {
+                    print("Button: \(item.title)")
+                }
+            }
+                
         }
         .padding()
     }
@@ -22,5 +28,24 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CalcularotButton: View {
+    let fontSize: CGFloat = 38
+    let title: String
+    let size: CGSize
+    let backgroundColorName: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action, label: {
+            Text(title)
+                .font(.system(size: fontSize))
+                .foregroundColor(.white)
+                .frame(width: size.width,height: size.height)
+                .background(Color(backgroundColorName))
+                .cornerRadius(size.width/2)
+        })
     }
 }
